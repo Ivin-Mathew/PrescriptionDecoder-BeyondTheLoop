9f7873bdeddc1e52ef0240c2c4bd9880a42fb97d
Ivin <ivinmk2410@hotmail.com>
2024-04-03 14:19:25 +0530

    New app frame created

---
+++ prescription_decoder/README.md
@@ -0,0 +1,3 @@
+# prescription_decoder
+
+A new Flutter project.
---
+++ prescription_decoder/android/app/src/main/kotlin/com/example/prescription_decoder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prescription_decoder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
